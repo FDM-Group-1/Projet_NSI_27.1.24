4b7c2879969f1964f689b9186cf22c9096571967
FDM-Group-1 <soshmarc@yahoo.com>
2025-01-27 10:16:46 -0300

    Update Partie-Marc.py

--- Partie-Marc.py
+++ Partie-Marc.py
@@ -1,12 +1,14 @@
-Liste_test = [{"joueur" : "MAM", "Jeux" : "Hand", "Score" : 1012},{"joueur" : "AAA", "Jeux" : "DDD", "Score" : 111}]
+liAA = [{"joueur" : "MAM", "Jeux" : "Hand", "Score" : 1012},{"joueur" : "AAA", "Jeux" : "DDD", "Score" : 111}]
 
 #Affichage des différents joueurs avec leurs noms, jeux et scores
 def jrs_stats (dico):
     compt = 0 #Compteur pour savoir le nombre de joueur total
-    print("Voici les différents joueurs avec leurs scores")
+    print("Voici les différents joueurs avec leurs scores :")
+    print("----------------------------------------------")
     for valeurs in dico:
         compt = compt +1
-        print(f"Joueur ({compt}) : {valeurs["joueur"]}, Jeux : {valeurs["Jeux"]}, Score : {valeurs["Score"]}") 
-    return f"nombre de joueurs : {compt}" #Nombre total de joueurs
+        print(f"Joueur ({compt}) : {valeurs["joueur"]},  Jeux : {valeurs["Jeux"]},  Score : {valeurs["Score"]} ") 
+    print("----------------------------------------------")
+    return f"nombre total de joueurs : {compt}" #Nombre total de joueurs
 
-print(jrs_stats(Liste_test))
+print(jrs_stats(liAA))
